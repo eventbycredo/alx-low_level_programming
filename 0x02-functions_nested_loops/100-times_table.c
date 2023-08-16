7893bac64c5e9c03ccfd366dc02d9a211a6502fc
eventbycredo <ekenecredo@gmail.com>
2023-08-16 04:29:01 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,44 @@
 #include "main.h"
 
 /**
- *print_times_table - prints the n times table
- *@n: number of the times table
+ * print_times_table - Prints the n times table
+ * @n: number times table (0 < n <= 15)
+ * Return: no return
  */
 void print_times_table(int n)
 {
-	int i, j, k;
+	int a, b, c;
 
 	if (n >= 0 && n <= 15)
 	{
-	for (i = 0; i <= n; i++)
+	for (a = 0; a <= n; a++)
 	{
-	for (j = 0; j <= n; j++)
+	_putchar(48);
+	for (b = 1; b <= n; b++)
 	{
-	k = j * i;
-	if (j == 0)
+	c = a * b;
+	_putchar(44);
+	_putchar(32);
+	if (c <= 9)
 	{
-	_putchar(k + '0');
+	_putchar(32);
+	_putchar(32);
+	_putchar(c + 48);
 	}
-	else if (k < 10 && j != 0)
+	else if (c <= 99)
 	{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(k + '0');
+	_putchar(32);
+	_putchar((c / 10) + 48);
+	_putchar((c % 10) + 48);
 	}
-	else if (k >= 10 && k < 100)
+	else
 	{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar((k / 10) + '0');
-	_putchar((k % 10) + '0');
-	}
+	_putchar(((c / 100) % 10) + 48);
+	_putchar(((c / 10) % 10) + 48);
+	_putchar((c % 10) + 48);
 	}
 	}
 	_putchar('\n');
+	}
 	}
 }
